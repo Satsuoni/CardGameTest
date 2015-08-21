41dd80d17b80b7dad7120b08cbf16dda5e2bb900
Seva <who@cares.com>
2015-08-21 17:42:57 +0900

    Thoughts on progs

--- Assets/Scripts/Temptest.cs
+++ Assets/Scripts/Temptest.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+
 public class base1
 {
 }
@@ -12,12 +14,20 @@ public class Temptest : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		TextAsset ta=Resources.Load("utftest") as TextAsset;
+		//MemoryStream stream = new MemoryStream(ta.bytes);
+		string st=System.Text.Encoding.UTF8.GetString(ta.bytes);
 
+		Debug.Log(char.IsWhiteSpace(st[0]));
+		Debug.Log('\n');
 		List<check1> ch=new List<check1>();
 		ch.Add(new check1());
 		Debug.Log(ch is IList);
 		foreach(object o in(IList) ch)
 			Debug.Log(o);
+
+
+
 	}
 	
 	// Update is called once per frame
